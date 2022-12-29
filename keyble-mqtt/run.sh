9dbde1b310b7b2c7075420749dd0a4535c0c7d4b
Markus Mauch <markus.mauch@gmail.com>
2022-12-29 15:12:58 +0100

    keyble

---
+++ keyble-mqtt/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
+
+ADDRESS="$(jq --raw-output '.address' $CONFIG_PATH)"
+USER_ID="$(jq --raw-output '.user_id' $CONFIG_PATH)"
+USER_KEY="$(jq --raw-output '.user_key' $CONFIG_PATH)"
+USERNAME="$(jq --raw-output '.username' $CONFIG_PATH)"
+PASSWORD="$(jq --raw-output '.password' $CONFIG_PATH)"
+
+echo ADDRESS: $ADDRESS
+echo USER_ID: $USER_ID
+echo USER_KEY: $USER_KEY
+echo USERNAME: $USERNAME
+echo PASSWORD: $PASSWORD
+
+# keyble-mqtt $ADDRESS $USER_ID $USER_KEY --username $USERNAME --password $PASSWORD
+tail -f /dev/null
