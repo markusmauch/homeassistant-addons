b369e3a0eefaa4e363a32690c515fe65187dd920
Markus Mauch <markus.mauch@gmail.com>
2023-01-10 15:03:49 +0100

    add host

--- keyble-mqtt/run.sh
+++ keyble-mqtt/run.sh
@@ -7,14 +7,16 @@ CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
 ADDRESS="$(jq --raw-output '.address' $CONFIG_PATH)"
 USER_ID="$(jq --raw-output '.user_id' $CONFIG_PATH)"
 USER_KEY="$(jq --raw-output '.user_key' $CONFIG_PATH)"
+HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
 USERNAME="$(jq --raw-output '.username' $CONFIG_PATH)"
 PASSWORD="$(jq --raw-output '.password' $CONFIG_PATH)"
 
-echo ADDRESS: $ADDRESS
-echo USER_ID: $USER_ID
-echo USER_KEY: $USER_KEY
-echo USERNAME: $USERNAME
-echo PASSWORD: $PASSWORD
+# echo ADDRESS: $ADDRESS
+# echo USER_ID: $USER_ID
+# echo USER_KEY: $USER_KEY
+# echo HOST: $HOST
+# echo USERNAME: $USERNAME
+# echo PASSWORD: $PASSWORD
 
-# keyble-mqtt $ADDRESS $USER_ID $USER_KEY --username $USERNAME --password $PASSWORD
+keyble-mqtt $ADDRESS $USER_ID $USER_KEY --host $HOST--username $USERNAME --password $PASSWORD
 tail -f /dev/null
