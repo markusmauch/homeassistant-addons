8cb852e430bb3fcfe0892cd44440b6afe39c5c1a
Markus Mauch <markus.mauch@sick.de>
2023-02-21 15:27:57 +0100

    mqttx

---
+++ mqttx/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
+
+HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
+USERNAME="$(jq --raw-output '.username' $CONFIG_PATH)"
+PASSWORD="$(jq --raw-output '.password' $CONFIG_PATH)"
+
+cd ./app
+http-server -p 80
+
+# node ./out/Index.js --host $HOST --username $USERNAME --password $PASSWORD --address $ADDRESS --user_id $USER_ID --user_key $USER_KEY
+# keyble-mqtt $ADDRESS $USER_ID $USER_KEY --host $HOST --username $USERNAME --password $PASSWORD
+# tail -f /dev/null
