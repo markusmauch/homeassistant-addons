206dee2529095cad38c865a48260b6543fa11e95
Markus Mauch <markus.mauch@sick.de>
2023-02-23 17:46:29 +0100

    asdfasdf

--- keyble-mqtt/src/Index.ts
+++ keyble-mqtt/src/Index.ts
@@ -95,7 +95,7 @@ mqttClient.on( "connect", async () =>
     );
 
     publishCurrentState();
-    job = schedule.scheduleJob( `*/${POLL_INTERVAL} * * * * *`, () => queue.enqueue( async () => publishCurrentState() ) );
+    job = schedule.scheduleJob( `* */10 * * * *`, () => queue.enqueue( async () => publishCurrentState() ) );
 
     mqttClient.subscribe( subscription, {qos: 0}, ( err, res ) =>
     {
