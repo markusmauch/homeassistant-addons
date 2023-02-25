914ca3919a8d13d57c1f73ca1adf4890e8141c48
Markus Mauch <markus.mauch@gmail.com>
2023-02-25 17:23:07 +0100

    dfg

--- keyble-mqtt/run.sh
+++ keyble-mqtt/run.sh
@@ -13,14 +13,14 @@ set -e
 # AUTO_DISCONNECT_TIME="$(jq --raw-output '.auto_disconnect_time' $CONFIG_PATH)"
 # POLL_INTERVAL="$(jq --raw-output '.poll_interval' $CONFIG_PATH)"
 
-ADDRESS="$(bashio::config 'address')"
-USER_ID="$(bashio::config 'user_id')"
-USER_KEY="$(bashio::config 'user_key')"
+ADDRESS=$(bashio::config "address")
+USER_ID=$(bashio::config "user_id")
+USER_KEY=$(bashio::config "user_key")
 HOST=$(bashio::services mqtt "host")
 USERNAME=$(bashio::services mqtt "username")
 PASSWORD=$(bashio::services mqtt "password")
-AUTO_DISCONNECT_TIME="$(bashio::config 'auto_disconnect_time')"
-POLL_INTERVAL="$(bashio::config 'poll_interval')"
+AUTO_DISCONNECT_TIME=$(bashio::config "auto_disconnect_time")
+POLL_INTERVAL=$(bashio::config "poll_interval")
 
 ./node_modules/typescript/bin/tsc
 node ./out/Index.js --host $HOST --username $USERNAME --password $PASSWORD --address $ADDRESS --user_id $USER_ID --user_key $USER_KEY --auto_disconnect_time $AUTO_DISCONNECT_TIME --poll_interval $POLL_INTERVAL
