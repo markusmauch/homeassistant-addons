c1e198b4cac8f67b2decc915874a923411fd3b08
Markus Mauch <markus.mauch@gmail.com>
2023-11-20 11:52:43 +0100

    test

--- example/run.sh
+++ example/run.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::cache.flush_all
-hostname="hassio"
-if bashio::supervisor.ping; then
-        hostname=${bashio::host.hostname}
-fi
+echo "OK"
 
-echo "> ${hostname}"
 
-USERNAME=$(bashio::config 'mqtt_username')
-bashio::log.info "${USERNAME}"
+# bashio::cache.flush_all
+# hostname="hassio"
+# if bashio::supervisor.ping; then
+#         hostname=${bashio::host.hostname}
+# fi
+
+# echo "> ${hostname}"
+
+# USERNAME=$(bashio::config 'mqtt_username')
+# bashio::log.info "${USERNAME}"
