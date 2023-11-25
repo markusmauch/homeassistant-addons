76e3bc9d0d4a37b50a55b1838f862eb575500744
Markus Mauch <markus.mauch@gmail.com>
2023-11-25 14:58:06 +0100

    mqtt announcement

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bashio
-echo `pwd`
+
+# Define the configuration for the sensor
+SENSOR_CONFIG='{
+  "name": "Balkonsolar Total Photovoltaic Power",
+  "state_topic": "solix/site/Balkonsolar/scenInfo",
+  "value_template": "{{ value_json.solarbank_info.total_photovoltaic_power }}",
+  "unit_of_measurement": "W",
+  "device": {
+    "identifiers": ["balkonsolar"],
+    "name": "Balkonsolar",
+    "model": "Solarbank E1600",
+    "manufacturer": "YourManufacturer"
+  }
+}'
+
+# Define the MQTT topic for Home Assistant auto-discovery
+DISCOVERY_TOPIC="homeassistant/sensor/balkonsolar_total_photovoltaic_power/config"
+
+# Publish the sensor configuration to the MQTT broker
+bashio::mqtt.publish -t "$DISCOVERY_TOPIC" -m "$SENSOR_CONFIG" -r
+
+# Log a message indicating that the sensor has been announced
+bashio::log.info "Balkonsolar Total Photovoltaic Power sensor has been announced to Home Assistant."
+
+
+
 node ./bin/app.js
