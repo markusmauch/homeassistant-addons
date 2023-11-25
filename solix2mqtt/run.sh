3a50c19abb75a91f2bcc154c7518faa2523a9711
Markus Mauch <markus.mauch@gmail.com>
2023-11-25 14:43:18 +0100

    sadfg

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bashio
-
-USERNAME=$(bashio::config 'username')
-
-bashio::log.info "${USERNAME}"
-# bashio::log.info "$(bashio::api 'GET' '/os/info')"
-bashio::log.info "$(bashio::network)"
 echo `pwd`
-deno run https://examples.deno.land/hello-world.ts
-
-deno run --allow-read --allow-net --allow-env /app/index.ts
-
-# pm2 start ping -- localhost
-# pm2 monit
+node ./bin/app.js
