766048e3c2d89988884b78fe00c63000bde58c83
Markus Mauch <markus.mauch@gmail.com>
2023-11-25 15:25:12 +0100

    asdf

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bashio
 
 # Define the configuration for the sensor
-SENSOR_CONFIG='{
-  "name": "Balkonsolar Total Photovoltaic Power",
-  "state_topic": "solix/site/Balkonsolar/scenInfo",
-  "value_template": "{{ value_json.solarbank_info.total_photovoltaic_power }}",
-  "unit_of_measurement": "W",
-  "device": {
-    "identifiers": ["balkonsolar"],
-    "name": "Balkonsolar",
-    "model": "Solarbank E1600",
-    "manufacturer": "YourManufacturer"
-  }
-}'
-
-# Remove newlines and spaces to make the JSON a single line
-SENSOR_CONFIG=$(echo "$SENSOR_CONFIG" | tr -d '\n' | tr -d ' ')
+SENSOR_CONFIG=$(bashio::var.json \
+  name "Balkonsolar Total Photovoltaic Power" \
+  state_topic "solix/site/Balkonsolar/scenInfo" \
+  value_template "{{ value_json.solarbank_info.total_photovoltaic_power }}" \
+  unit_of_measurement "W" \
+  device '{"identifiers": ["balkonsolar"], "name": "Balkonsolar", "model": "Solarbank E1600", "manufacturer": "YourManufacturer"}'
+)
 
 # Announce the sensor to Home Assistant using bashio::discovery
-bashio::discovery "sensor" "balkonsolar_total_photovoltaic_power" "$SENSOR_CONFIG"
+if bashio::discovery "sensor" "balkonsolar_total_photovoltaic_power" "${SENSOR_CONFIG}" > /dev/null; then
+  bashio::log.info "Successfully sent discovery information to Home Assistant."
+else
+  bashio::log.error "Discovery message to Home Assistant failed!"
+fi
 
-# Log a message indicating that the sensor has been announced
-bashio::log.info "Balkonsolar Total Photovoltaic Power sensor has been announced to Home Assistant."
 
 node ./bin/app.js
