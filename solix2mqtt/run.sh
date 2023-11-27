d4fa1b5d43f9d97927cab7ade93125a5f824a257
Markus Mauch <markus.mauch@gmail.com>
2023-11-27 19:10:31 +0000

    sdfgsdfg

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -9,14 +9,44 @@ export S2M_MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
 export S2M_MQTT_URI="mqtt://$S2M_MQTT_HOST:1883"
 # export S2M_MQTT_TOPIC="homeassistant/"
 
-TOPIC="homeassistant/sensor/solarbank_e1600/battery_power/config"
+TOPIC="homeassistant/sensor/solarbank_e1600/battery_level/config"
 PAYLOAD=$(echo '{
-  "name": "Solarbank E1600 Battery Power",
+  "name": "Solarbank E1600 Battery Level",
   "state_topic": "solix/site/Balkonsolar/scenInfo",
-  "value_template": "{{ value_json.solarbank_info.solarbank_list[0].battery_power | float }}",
+  "value_template": "{{ value_json.solarbank_info.total_battery_power | float * 100 }}",
   "device_class": "battery",
+  "unit_of_measurement": "%",
+  "unique_id": "solarbank_e1600_battery_level"
+}' | jq -c '.')
+
+TOPIC="homeassistant/sensor/solarbank_e1600/photovoltaic_power/config"
+PAYLOAD=$(echo '{
+  "name": "Solarbank E1600 Photovoltaic Power",
+  "state_topic": "solix/site/Balkonsolar/scenInfo",
+  "value_template": "{{ value_json.solarbank_info.total_photovoltaic_power | float }}",
+  "device_class": "power",
   "unit_of_measurement": "W",
-  "unique_id": "solarbank_e1600_battery_power"
+  "unique_id": "solarbank_e1600_photovoltaic_power"
+}' | jq -c '.')
+
+TOPIC="homeassistant/sensor/solarbank_e1600/output_power/config"
+PAYLOAD=$(echo '{
+  "name": "Solarbank E1600 Output Power",
+  "state_topic": "solix/site/Balkonsolar/scenInfo",
+  "value_template": "{{ value_json.solarbank_info.total_output_power | float }}",
+  "device_class": "power",
+  "unit_of_measurement": "W",
+  "unique_id": "solarbank_e1600_output_power"
+}' | jq -c '.')
+
+TOPIC="homeassistant/sensor/solarbank_e1600/charging_power/config"
+PAYLOAD=$(echo '{
+  "name": "Solarbank E1600 Charging Power",
+  "state_topic": "solix/site/Balkonsolar/scenInfo",
+  "value_template": "{{ value_json.solarbank_info.total_charging_power | float }}",
+  "device_class": "power",
+  "unit_of_measurement": "W",
+  "unique_id": "solarbank_e1600_charging_power"
 }' | jq -c '.')
 
 
