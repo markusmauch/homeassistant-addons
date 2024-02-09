73988fa11d1e0f4eb5fefd75694d874f1aa03e54
Markus Mauch <markus.mauch@gmail.com>
2024-02-09 11:51:42 +0100

    - replace mqtt_host with mqtt_uri - Update readme

--- solix2mqtt/README.md
+++ solix2mqtt/README.md
@@ -1,3 +1,25 @@
 # Anker Solix Solarbank E1600 to MQTT
 
 This addon uses the [solix2mqtt](https://github.com/tomquist/solix2mqtt) library to poll the Solix API for the latest sample data and publish them to an MQTT broker.
+
+## Configuration
+
+The app can be configured using the following parameters:
+
+__user__: A Solix API client id
+
+__password__: The client secret
+
+__country__: A two-letter country code (e.g. DE)
+
+__mqtt_uri__: The MQTT broker URL, e.g. mqtt://homeassistant.local:1883
+
+__mqtt_username__: Optional username for MQTT authentication
+
+__mqtt_password__: Optional password for MQTT authentication
+
+## Breaking changes
+
+### Version 1.2.0
+
+- Configuration parameter mqtt_host has been replaced by mqtt_uri. So it is now possible __and required__ to also specify the protocol and port.
--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -3,60 +3,97 @@
 export S2M_USER="$(bashio::config 'user')"
 export S2M_PASSWORD="$(bashio::config 'password')"
 export S2M_COUNTRY="$(bashio::config 'country')"
-export S2M_MQTT_HOST="$(bashio::config 'mqtt_host')"
+export S2M_MQTT_URI="$(bashio::config 'mqtt_uri')"
 export S2M_MQTT_USERNAME="$(bashio::config 'mqtt_username')"
 export S2M_MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+export S2M_MQTT_TOPIC="$(bashio::config 'mqtt_topic')"
 export S2M_POLL_INTERVAL="$(bashio::config 'poll_interval')"
-export S2M_MQTT_URI="mqtt://$S2M_MQTT_HOST:1883"
 export S2M_VERBOSE=true
-# export S2M_MQTT_TOPIC="homeassistant/"
 
 # Function to publish MQTT messages for a sensor
 publish_sensor() {
-  local topic="$1"
-  local name="$2"
-  local unique_id="$3"
-  local value_template="$4"
-  local device_class="${5:-null}"
-  local unit_of_measurement="${6:-null}"
-  local state_topic="solix/site/Balkonsolar/scenInfo"
-  local payload=$(jq -c -n \
-    --arg name "$name" \
-    --arg topic "$topic" \
-    --arg state_topic "$state_topic" \
-    --arg value_template "$value_template" \
-    --arg device_class "$device_class" \
-    --arg unit_of_measurement "$unit_of_measurement" \
-    --arg unique_id "$unique_id" \
-    '{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
-  )
-  echo Announcing entity: \'$name\' with payload
-  echo $payload
-  mosquitto_pub -h "$S2M_MQTT_HOST" -u "$S2M_MQTT_USERNAME" -P "$S2M_MQTT_PASSWORD" -t "$topic" -m "$payload" --retain
-  echo "Done."
-  echo ""
+	local topic="$1"
+	local name="$2"
+	local unique_id="$3"
+	local value_template="$4"
+	local device_class="${5:-null}"
+	local unit_of_measurement="${6:-null}"
+	local state_topic=$S2M_MQTT_TOPIC
+	local payload=$(
+		jq -c -n \
+			--arg name "$name" \
+			--arg topic "$topic" \
+			--arg state_topic "$state_topic" \
+			--arg value_template "$value_template" \
+			--arg device_class "$device_class" \
+			--arg unit_of_measurement "$unit_of_measurement" \
+			--arg unique_id "$unique_id" \
+			'{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
+	)
+	echo Announcing entity: \'$name\' with payload
+	echo $payload
+	mosquitto_pub -h "$S2M_MQTT_HOST" -u "$S2M_MQTT_USERNAME" -P "$S2M_MQTT_PASSWORD" -t "$topic" -m "$payload" --retain
+	echo "Done."
+	echo ""
 }
 
 # Publish battery level sensor
-publish_sensor "homeassistant/sensor/solarbank_e1600/battery_level/config" "Solarbank E1600 Battery Level" "solarbank_e1600_battery_level" "{{ value_json.solarbank_info.total_battery_power | float * 100 }}" "battery" "%"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/battery_level/config" \
+	"Solarbank E1600 Battery Level" \
+	"solarbank_e1600_battery_level" \
+	"{{ value_json.solarbank_info.total_battery_power | float * 100 }}" \
+	"battery" \
+	"%"
 
 # Publish photovoltaic power sensor
-publish_sensor "homeassistant/sensor/solarbank_e1600/photovoltaic_power/config" "Solarbank E1600 Photovoltaic Power" "solarbank_e1600_photovoltaic_power" "{{ value_json.solarbank_info.total_photovoltaic_power | float }}" "power" "W"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/photovoltaic_power/config" \
+	"Solarbank E1600 Photovoltaic Power" \
+	"solarbank_e1600_photovoltaic_power" \
+	"{{ value_json.solarbank_info.total_photovoltaic_power | float }}" \
+	"power" \
+	"W"
 
 # Publish photovoltaic yield sensor
-publish_sensor "homeassistant/sensor/solarbank_e1600/photovoltaic_yield/config" "Solarbank E1600 Photovoltaic Yield" "solarbank_e1600_photovoltaic_yield" "{{ value_json.statistics[0].total | float }}" "energy" "kWh"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/photovoltaic_yield/config" \
+	"Solarbank E1600 Photovoltaic Yield" \
+	"solarbank_e1600_photovoltaic_yield" \
+	"{{ value_json.statistics[0].total | float }}" \
+	"energy" \
+	"kWh"
 
 # Publish output power sensor
-publish_sensor "homeassistant/sensor/solarbank_e1600/output_power/config" "Solarbank E1600 Output Power" "solarbank_e1600_output_power" "{{ value_json.solarbank_info.total_output_power | float }}" "power" "W"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/output_power/config" \
+	"Solarbank E1600 Output Power" \
+	"solarbank_e1600_output_power" \
+	"{{ value_json.solarbank_info.total_output_power | float }}" \
+	"power" \
+	"W"
 
 # Publish charging power sensor
-publish_sensor "homeassistant/sensor/solarbank_e1600/charging_power/config" "Solarbank E1600 Charging Power" "solarbank_e1600_charging_power" "{{ value_json.solarbank_info.total_charging_power | float }}" "power" "W"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/charging_power/config" \
+	"Solarbank E1600 Charging Power" \
+	"solarbank_e1600_charging_power" \
+	"{{ value_json.solarbank_info.total_charging_power | float }}" \
+	"power" \
+	"W"
 
 # Publish last update
-publish_sensor "homeassistant/sensor/solarbank_e1600/last_update/config" "Solarbank E1600 Last Update" "solarbank_e1600_last_update" "{{ value_json.solarbank_info.updated_time }}"
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/last_update/config" \
+	"Solarbank E1600 Last Update" \
+	"solarbank_e1600_last_update" \
+	"{{ value_json.solarbank_info.updated_time }}"
 
 # Publish charging status
-publish_sensor "homeassistant/sensor/solarbank_e1600/charging_status/config" "Solarbank E1600 Charging Status" "solarbank_e1600_charging_status" "{{ value_json.solarbank_info.charging_status }}"
-
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/charging_status/config" \
+	"Solarbank E1600 Charging Status" \
+	"solarbank_e1600_charging_status" \
+	"{{ value_json.solarbank_info.charging_status }}"
 
 node ./bin/app.js
