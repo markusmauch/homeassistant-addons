af535f9a32a168c2da328eceb5719f1878d46307
Markus Mauch <markus.mauch@gmail.com>
2024-02-11 14:31:54 +0100

    Schedule

--- solix2mqtt/CHANGELOG.md
+++ solix2mqtt/CHANGELOG.md
@@ -5,6 +5,7 @@
 - mqtt_host has been replaced by mqtt_uri
 - Added mqtt_topic to use a custom topic
 - Added statistics
+- Added schedule (read-only)
 
 ## Breaking changes
 - Replacement of parameter mqtt_host requires to specify the complete host URI (https://datatracker.ietf.org/doc/html/rfc3986).
--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -23,16 +23,18 @@ publish_sensor() {
 	local topic="$1"
 	local name="$2"
 	local unique_id="$3"
-	local value_template="$4"
-	local device_class="${5:-null}"
-	local unit_of_measurement="${6:-null}"
+	local dataSet="$4"
+	local value_template="$5"
+	local device_class="${6:-null}"
+	local unit_of_measurement="${7:-null}"
+	local attribute_template="${8:-null}"
 
 	local username=$(parse_uri $S2M_MQTT_URI username)
 	local password=$(parse_uri $S2M_MQTT_URI password)
 	local scheme=$(parse_uri $S2M_MQTT_URI scheme)
 	local hostname=$(parse_uri $S2M_MQTT_URI hostname)
 	local port=$(parse_uri $S2M_MQTT_URI port)
-	local state_topic=$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo
+	local state_topic=$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/$dataSet
 
 	local payload=$(
 		jq -c -n \
@@ -43,7 +45,7 @@ publish_sensor() {
 			--arg device_class "$device_class" \
 			--arg unit_of_measurement "$unit_of_measurement" \
 			--arg unique_id "$unique_id" \
-			'{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
+			'{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "attribute_template": $attribute_template, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
 	)
 	echo Announcing entity \'$name\' to host: "$scheme"://"$hostname:${port:-1883}" with payload
 	echo $payload
@@ -57,6 +59,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/battery_level/config" \
 	"Solarbank E1600 Battery Level"\
 	"solarbank_e1600_battery_level" \
+	"scenInfo" \
 	"{{ ( value_json.solarbank_info.total_battery_power | float ) * 100 }}" \
 	"battery" \
 	"%"
@@ -66,6 +69,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/photovoltaic_power/config" \
 	"Solarbank E1600 Photovoltaic Power" \
 	"solarbank_e1600_photovoltaic_power" \
+	"scenInfo" \
 	"{{ value_json.solarbank_info.total_photovoltaic_power | float }}" \
 	"power" \
 	"W"
@@ -75,6 +79,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/photovoltaic_yield/config" \
 	"Solarbank E1600 Photovoltaic Yield" \
 	"solarbank_e1600_photovoltaic_yield" \
+	"scenInfo" \
 	"{{ value_json.statistics[0].total | float }}" \
 	"energy" \
 	"kWh"
@@ -84,6 +89,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/output_power/config" \
 	"Solarbank E1600 Output Power" \
 	"solarbank_e1600_output_power" \
+	"scenInfo" \
 	"{{ value_json.solarbank_info.total_output_power | float }}" \
 	"power" \
 	"W"
@@ -93,6 +99,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/charging_power/config" \
 	"Solarbank E1600 Charging Power" \
 	"solarbank_e1600_charging_power" \
+	"scenInfo" \
 	"{{ value_json.solarbank_info.total_charging_power | float }}" \
 	"power" \
 	"W"
@@ -102,6 +109,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/last_update/config" \
 	"Solarbank E1600 Last Update" \
 	"solarbank_e1600_last_update" \
+	"scenInfo" \
 	"{{ value_json.solarbank_info.updated_time }}"
 
 # Publish charging status
@@ -109,6 +117,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/charging_status/config" \
 	"Solarbank E1600 Charging Status" \
 	"solarbank_e1600_charging_status" \
+	"scenInfo" \
 	"{{ value_json.solarbank_info.solarbank_list[0].charging_status }}"
 
 # Publish statistics yield
@@ -116,6 +125,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_yield/config" \
 	"Solarbank E1600 Statistics Yield" \
 	"solarbank_e1600_statistics_yield" \
+	"scenInfo" \
 	"{{ value_json.statistics[0].total }}" \
 	"energy" \
 	"kWh"
@@ -125,6 +135,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_co2_savings/config" \
 	"Solarbank E1600 Statistics CO2 Savings" \
 	"solarbank_e1600_statistics_co2_savings" \
+	"scenInfo" \
 	"{{ value_json.statistics[1].total }}" \
 	"weight" \
 	"kg"
@@ -134,8 +145,20 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_saved_costs/config" \
 	"Solarbank E1600 Charging Statistics Saved Costs" \
 	"solarbank_e1600_statistics_saved_costs" \
+	"scenInfo" \
 	"{{ value_json.statistics[2].total }}" \
 	"monetary" \
-	"{{ 'EUR' if value_json.statistics[2].unit = '€' else 'USD' if value_json.statistics[2].unit = '$' else '' }}"
+	"EUR"
+
+# Publish schedule
+publish_sensor \
+	"homeassistant/sensor/solarbank_e1600/schedule/config" \
+	"Solarbank E1600 Schedule" \
+	"solarbank_e1600_schedule" \
+	"schedule" \
+	"{{value_json.ranges|length}}" \
+	null \
+	null \
+	"{{'attribute_templates:': value_json.ranges}}"
 
 node ./index.js
