212263abf17bb546a7af2794565c5a3f37edabdd
Markus Mauch <markus.mauch@gmail.com>
2024-02-11 15:29:09 +0100

    asdfsadf

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -23,30 +23,29 @@ publish_sensor() {
 	local topic="$1"
 	local name="$2"
 	local unique_id="$3"
-	local dataSet="$4"
+	local state_topic="$4"
 	local value_template="$5"
 	local device_class="${6:-null}"
 	local unit_of_measurement="${7:-null}"
-	local attribute_template="${8:-null}"
+	local json_attributes_topic="${8:-null}"
 
 	local username=$(parse_uri $S2M_MQTT_URI username)
 	local password=$(parse_uri $S2M_MQTT_URI password)
 	local scheme=$(parse_uri $S2M_MQTT_URI scheme)
 	local hostname=$(parse_uri $S2M_MQTT_URI hostname)
 	local port=$(parse_uri $S2M_MQTT_URI port)
-	local state_topic=$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/$dataSet
 
 	local payload=$(
 		jq -c -n \
+			--arg unique_id "$unique_id" \
 			--arg name "$name" \
 			--arg topic "$topic" \
 			--arg state_topic "$state_topic" \
 			--arg value_template "$value_template" \
 			--arg device_class "$device_class" \
 			--arg unit_of_measurement "$unit_of_measurement" \
-			--arg unique_id "$unique_id" \
-			--arg attribute_template "$attribute_template" \
-			'{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "attribute_template": $attribute_template, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
+			--arg json_attributes_topic "$json_attributes_topic" \
+			'{"name": $name, "state_topic": $state_topic, "value_template": $value_template, "json_attributes_topic": $json_attributes_topic, "device_class": $device_class, "unit_of_measurement": $unit_of_measurement, "unique_id": $unique_id} | with_entries(select(.value!="null"))'
 	)
 	echo Announcing entity \'$name\' to host: "$scheme"://"$hostname:${port:-1883}" with payload
 	echo $payload
@@ -60,7 +59,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/battery_level/config" \
 	"Solarbank E1600 Battery Level"\
 	"solarbank_e1600_battery_level" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ ( value_json.solarbank_info.total_battery_power | float ) * 100 }}" \
 	"battery" \
 	"%"
@@ -70,7 +69,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/photovoltaic_power/config" \
 	"Solarbank E1600 Photovoltaic Power" \
 	"solarbank_e1600_photovoltaic_power" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.solarbank_info.total_photovoltaic_power | float }}" \
 	"power" \
 	"W"
@@ -80,7 +79,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/photovoltaic_yield/config" \
 	"Solarbank E1600 Photovoltaic Yield" \
 	"solarbank_e1600_photovoltaic_yield" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.statistics[0].total | float }}" \
 	"energy" \
 	"kWh"
@@ -90,7 +89,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/output_power/config" \
 	"Solarbank E1600 Output Power" \
 	"solarbank_e1600_output_power" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.solarbank_info.total_output_power | float }}" \
 	"power" \
 	"W"
@@ -100,7 +99,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/charging_power/config" \
 	"Solarbank E1600 Charging Power" \
 	"solarbank_e1600_charging_power" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.solarbank_info.total_charging_power | float }}" \
 	"power" \
 	"W"
@@ -110,7 +109,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/last_update/config" \
 	"Solarbank E1600 Last Update" \
 	"solarbank_e1600_last_update" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.solarbank_info.updated_time }}"
 
 # Publish charging status
@@ -118,7 +117,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/charging_status/config" \
 	"Solarbank E1600 Charging Status" \
 	"solarbank_e1600_charging_status" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.solarbank_info.solarbank_list[0].charging_status }}"
 
 # Publish statistics yield
@@ -126,7 +125,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_yield/config" \
 	"Solarbank E1600 Statistics Yield" \
 	"solarbank_e1600_statistics_yield" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.statistics[0].total }}" \
 	"energy" \
 	"kWh"
@@ -136,7 +135,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_co2_savings/config" \
 	"Solarbank E1600 Statistics CO2 Savings" \
 	"solarbank_e1600_statistics_co2_savings" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.statistics[1].total }}" \
 	"weight" \
 	"kg"
@@ -146,7 +145,7 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/statistics_saved_costs/config" \
 	"Solarbank E1600 Charging Statistics Saved Costs" \
 	"solarbank_e1600_statistics_saved_costs" \
-	"scenInfo" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/scenInfo \
 	"{{ value_json.statistics[2].total }}" \
 	"monetary" \
 	"EUR"
@@ -156,10 +155,10 @@ publish_sensor \
 	"homeassistant/sensor/solarbank_e1600/schedule/config" \
 	"Solarbank E1600 Schedule" \
 	"solarbank_e1600_schedule" \
-	"schedule" \
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/schedule \
 	"{{value_json.ranges|length}}" \
 	null \
 	null \
-	"{{'complex_object_list': value_json.ranges}}"
+	$S2M_MQTT_TOPIC/site/$S2M_SITE_NAME/schedule
 
 node ./index.js
