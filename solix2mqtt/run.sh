f9ffb1d2a42651c6ff4a2907d45e60e91a7d3d64
Markus Mauch <markus.mauch@gmail.com>
2024-02-21 14:05:25 +0000

    sdfg

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -7,7 +7,7 @@ export S2M_MQTT_URI="$(bashio::config 'mqtt_uri')"
 export S2M_MQTT_USERNAME="$(bashio::config 'mqtt_username')"
 export S2M_MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
 export S2M_MQTT_TOPIC="$(bashio::config 'mqtt_topic')"
-export S2M_POLL_INTERVAL="$(bashio::config 'poll_interval')"
+export S2M_POLL_INTERVAL=$(bashio::config 'poll_interval')
 export S2M_VERBOSE=true
 
 python3 solix2mqtt.py
