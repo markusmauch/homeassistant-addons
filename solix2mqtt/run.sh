2a4584533ce059d92d0938d0b61c9a53d1a717f1
Markus Mauch <markus.mauch@gmail.com>
2024-02-21 16:01:12 +0100

    sdfgsdfg

--- solix2mqtt/run.sh
+++ solix2mqtt/run.sh
@@ -13,14 +13,4 @@ export S2M_MQTT_TOPIC=$(jq --raw-output '.mqtt_topic // empty' $CONFIG_PATH)
 export S2M_POLL_INTERVAL=$(jq --raw-output '.poll_interval // 30' $CONFIG_PATH)
 export S2M_VERBOSE=true
 
-echo $S2M_USER
-echo $S2M_PASSWORD
-echo $S2M_COUNTRY
-echo $S2M_MQTT_URI
-echo $S2M_MQTT_USERNAME
-echo $S2M_MQTT_PASSWORD
-echo $S2M_MQTT_TOPIC
-echo $S2M_POLL_INTERVAL
-echo $S2M_VERBOSE
-
 python3 solix2mqtt.py
