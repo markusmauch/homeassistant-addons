7af90bb6fefecbca0a4ce9fe16a2370693c10b3f
Markus Mauch <markus.mauch@gmail.com>
2024-05-30 15:26:28 +0200

    test

---
+++ modbus2mqtt_old/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bashio
+
+echo "OK"
+
+
+# bashio::cache.flush_all
+# hostname="hassio"
+# if bashio::supervisor.ping; then
+#         hostname=${bashio::host.hostname}
+# fi
+
+# echo "> ${hostname}"
+
+# USERNAME=$(bashio::config 'mqtt_username')
+# bashio::log.info "${USERNAME}"
